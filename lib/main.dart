59e9b69166c83bc156184293e69b23e5741f89e8
ShokhjakhonRazokov <razzoqovabdulloh033@gmail.com>
2022-10-15 19:54:39 -0400

    About Companies project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:about_companies/screen/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
